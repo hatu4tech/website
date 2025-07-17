1aac218115914fa5d907fa6105b20f4050d93ffd
Maxw311 <nyimbilimaxwell9@gmail.com>
2025-07-17 10:58:26 +0200

    update email address

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -336,7 +336,7 @@ export default function HatuaTechWebsite() {
                 <Mail className="w-10 h-10 text-white" />
               </div>
               <h3 className="text-2xl font-semibold mb-4 text-gray-900">Email</h3>
-              <p className="text-gray-600 text-lg">hello@hatuatech.co.zm</p>
+              <p className="text-gray-600 text-lg">hello@hatuatech.com</p>
             </div>
           </div>
         </div>
